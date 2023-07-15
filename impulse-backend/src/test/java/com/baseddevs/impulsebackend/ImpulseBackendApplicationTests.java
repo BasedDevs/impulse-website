09dc23a276fd52822eb4d5959fcf36475e997ef7
Ognjen Prica <basedev0@gmail.com>
2023-07-15 16:29:55 +0200

    Initial commit

---
+++ impulse-backend/src/test/java/com/baseddevs/impulsebackend/ImpulseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baseddevs.impulsebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpulseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
